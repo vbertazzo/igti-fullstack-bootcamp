898c6aa02d9cc61cb94bba58b36dff042c146d6e
Vitor Bertazzo <vbertazzo@gmail.com>
2020-07-11 22:26:13 -0300

    feat: add search methods

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ let users = []
 window.addEventListener('load', async () => {
   const url =
     'https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo'
+  setupSearch()
   fetchUsersFrom(url)
 })
 
@@ -19,4 +20,35 @@ const fetchUsersFrom = async (url) => {
       picture: picture.thumbnail,
     }
   })
+  console.log(users)
+}
+
+const setupSearch = () => {
+  const searchBar = document.querySelector('#search-bar')
+  const searchButton = document.querySelector('#search-button')
+  searchBar.addEventListener('keyup', handleSearch)
+  searchButton.addEventListener('click', handleSearch)
+  searchBar.value = ''
+}
+
+const handleSearch = (event) => {
+  const { key } = event
+  const { value } = document.querySelector('#search-bar')
+  const searchButton = document.querySelector('#search-button')
+
+  value.length > 0
+    ? searchButton.removeAttribute('disabled')
+    : searchButton.setAttribute('disabled', true)
+
+  if (value.length > 0 || key === 'Enter') {
+    filterUsersByName(value)
+  }
+}
+
+const filterUsersByName = (name) => {
+  const result = users.filter((user) =>
+    user.name.toLowerCase().includes(name.toLowerCase()),
+  )
+
+  return result
 }
