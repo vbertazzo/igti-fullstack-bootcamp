3f51fb2c3352a529c62de8f1ed9b90978eb9b0c9
Vitor Bertazzo <vbertazzo@gmail.com>
2020-07-25 18:26:32 -0300

    feat: add create grade route

--- module-02/src/routes/grades.js
+++ module-02/src/routes/grades.js
@@ -1,16 +1,45 @@
 import express from 'express'
+import { promises as fs } from 'fs'
 
 const gradesRouter = express.Router()
 
-gradesRouter.get('/', (request, response, next) => {
+gradesRouter.post('/', async (request, response, next) => {
   try {
+    const { student, subject, type, value } = request.body
+
+    if (!(student && subject && type && value)) {
+      throw new Error('Missing parameters')
+    }
+
+    const gradesData = JSON.parse(await fs.readFile('./data/grades.json'))
+
+    const newGrade = {
+      id: gradesData.nextId,
+      student,
+      subject,
+      type,
+      value,
+      timestamp: new Date().toISOString(),
+    }
+
+    const newGrades = {
+      nextId: gradesData.nextId + 1,
+      grades: [...gradesData.grades, newGrade],
+    }
+
+    await fs.writeFile('./data/grades.json', JSON.stringify(newGrades))
+
     logger.info(
-      `${request.method} - ${request.baseUrl} - ${response.statusCode} - 'Ok'`
+      `${request.method} - ${
+        request.baseUrl
+      } [201] - Grade created successfully: ${JSON.stringify(newGrade)}`
     )
-    response.send('Ok')
+    response.status(201).json(newGrade)
   } catch (error) {
-    logger.error(error)
-    next(error)
+    logger.error(
+      `${request.method} - ${request.baseUrl} [400] - ${error.message}`
+    )
+    response.status(400).send({ error: 'missing parameters' })
   }
 })
 
